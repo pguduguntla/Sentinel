2567800e56037f5b1818057b087fa6fb567b4573
praneethguduguntla <praneethg347@gmail.com>
2018-06-03 12:38:15 -0700

    aye gang

--- app/src/main/java/com/example/praneethguduguntla/sentinel/OnStartActivity.java
+++ app/src/main/java/com/example/praneethguduguntla/sentinel/OnStartActivity.java
@@ -1,13 +1,42 @@
 package com.example.praneethguduguntla.sentinel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OnStartActivity extends AppCompatActivity {
+
+    private Button adminBtn, studentBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_start);
+
+        adminBtn = (Button) findViewById(R.id.ashish);
+        adminBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+        studentBtn = (Button) findViewById(R.id.studentButton);
+        studentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
+
+
+
+
 }
