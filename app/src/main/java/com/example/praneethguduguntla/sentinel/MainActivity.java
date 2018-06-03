da4197bd12c8ad6499c8ef38a474e63f4eedce30
praneethguduguntla <praneethg347@gmail.com>
2018-06-03 09:36:12 -0700

    aye gang

--- app/src/main/java/com/example/praneethguduguntla/sentinel/MainActivity.java
+++ app/src/main/java/com/example/praneethguduguntla/sentinel/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.praneethguduguntla.sentinel;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userName, emailField, userPassword;
+
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+       userName = (EditText) findViewById(R.id.schoolName);
+       emailField = (EditText) findViewById(R.id.email);
+       userPassword = (EditText) findViewById(R.id.password);
+
+       mAuth = FirebaseAuth.getInstance();
+
     }
+
+
+
+
+
 }
